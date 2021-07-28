5062d2ba2e481f02601d4f4a95df1a7c2da3c7ed
santushtisharma10 <sharmasantushti1012@gmail.com>
2021-07-28 17:40:06 +0530

    changes made

---
+++ list/src/components/ToDoList.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "./ToDoList.css"
+
+const TodoList = () => {
+
+    return(
+        <div>
+            <h1>To Do List</h1>
+            <button className="btn btn-primary">+</button>
+            <input></input>
+        </div>
+    )
+}
+
+export default TodoList
