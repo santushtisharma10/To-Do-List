9ca9bf6033272aeec309eab8a64c45ce3f65720c
santushtisharma10 <sharmasantushti1012@gmail.com>
2022-01-27 21:58:17 +0530

    changes made

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import './App.css';
 import TodoList from "./components/ToDoList";
+import Login from "./components/Login";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+
 
 function App() {
   return (
     <div className="App">
-      <TodoList />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<TodoList/>}/>
+          <Route exact path="/login" element={<Login/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
