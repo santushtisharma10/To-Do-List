f5b4870670800ba47f92e5822ec9f7ece82c6918
santushtisharma10 <sharmasantushti1012@gmail.com>
2022-01-28 13:22:10 +0530

    minor changes

--- frontend/src/components/ToDoList.js
+++ frontend/src/components/ToDoList.js
@@ -6,8 +6,6 @@ const TodoList = () => {
     return(
         <div>
             <h1>To Do List</h1>
-            <button className="btn btn-primary">+</button>
-            <input></input>
         </div>
     )
 }
